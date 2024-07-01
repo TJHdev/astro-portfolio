b3790e4a67ae0b78a45912d4ff809a48c7a51c64
Thomas <thomas@enhance.com>
2024-07-01 10:28:59 +0100

    improvements to header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import compressor from "astro-compressor";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), icon(), compressor()],
+  image: {
+    service: passthroughImageService(),
+  },
 });
